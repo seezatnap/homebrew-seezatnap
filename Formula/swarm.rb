a00c186f17d29db0d183a399dd800f0bce755921
seezatnap <seezatnap@gmail.com>
2026-02-04 14:32:26 -0600

    Fix

--- Formula/swarm.rb
+++ Formula/swarm.rb
@@ -2,7 +2,7 @@ class Swarm < Formula
   desc "CLI interface for agents working in sprints"
   homepage "https://github.com/seezatnap/swarm-hug"
   url "https://github.com/seezatnap/swarm-hug/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "e189b0eed02ba776b5b64e035b454fbea54a5e27a258e975da82b93e927db293"
   license "MIT"
 
   depends_on "rust" => :build

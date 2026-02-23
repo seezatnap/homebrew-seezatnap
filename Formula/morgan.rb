4d00a9efc7bcd5389acdcba14b43a89e97b01109
seezatnap <seezatnap@gmail.com>
2026-02-23 14:15:54 -0600

    Update

--- Formula/morgan.rb
+++ Formula/morgan.rb
@@ -1,8 +1,8 @@
 class Morgan < Formula
   desc "JulietScript generation and Juliet run orchestrator"
   homepage "https://github.com/seezatnap/morgan"
-  url "https://github.com/seezatnap/morgan/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "7ae24b9b02b460ac0f228822bb43750f1f79a8089f513867272dd037b904ebcb"
+  url "https://github.com/seezatnap/morgan/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "537c71576584d3ad45c06e6e1f2158e6d944a0b24ceb92ad90ab32897158ff91"
   license "MIT"
 
   depends_on "rust" => :build

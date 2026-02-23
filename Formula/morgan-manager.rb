f84fb62a7872944cbf190439c395f57e3a4578d2
seezatnap <seezatnap@gmail.com>
2026-02-23 14:14:59 -0600

    Add formula for morgan-manager

--- Formula/morgan-manager.rb
+++ Formula/morgan-manager.rb
@@ -1,19 +1,17 @@
-class Morgan < Formula
-  desc "JulietScript generation and Juliet run orchestrator"
+class MorganManager < Formula
+  desc "Process manager CLI for Morgan orchestrator runs"
   homepage "https://github.com/seezatnap/morgan"
   url "https://github.com/seezatnap/morgan/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "7ae24b9b02b460ac0f228822bb43750f1f79a8089f513867272dd037b904ebcb"
   license "MIT"
 
   depends_on "rust" => :build
-  depends_on "seezatnap/seezatnap/juliet"
-  depends_on "seezatnap/seezatnap/julietscript-lint"
 
   def install
-    system "cargo", "install", *std_cargo_args, "--bin", "morgan"
+    system "cargo", "install", *std_cargo_args, "--bin", "morgan-manager"
   end
 
   test do
-    system "#{bin}/morgan", "--help"
+    system "#{bin}/morgan-manager", "--help"
   end
 end

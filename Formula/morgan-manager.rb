4ca0a8ee71b0e98578ebefc45fa2d1305673044f
seezatnap <seezatnap@gmail.com>
2026-02-24 09:58:25 -0600

    Update

--- Formula/morgan-manager.rb
+++ Formula/morgan-manager.rb
@@ -1,8 +1,8 @@
 class MorganManager < Formula
   desc "Process manager CLI for Morgan orchestrator runs"
   homepage "https://github.com/seezatnap/morgan"
-  url "https://github.com/seezatnap/morgan/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "e7c73f98068683fdb8cc921d355ae8638e93aee9daae27261488278de9041564"
+  url "https://github.com/seezatnap/morgan/archive/refs/tags/v0.7.1.tar.gz"
+  sha256 "81ac304bdea94eb49e85dc12a8e96f26c33cea06f27d1c34f95bd16184b87c57"
   license "MIT"
 
   depends_on "rust" => :build
